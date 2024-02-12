33469715e60e5007fa111e725bacff38b8ede660
Juniper <juniperwrenarts@gmail.com>
2024-02-12 08:29:03 -0500

    added postRoutes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const { Post, User, Image } = require('../../models');
+const withAuth = require('../../utils/auth');
+const { myconfig } = require('../../config/config');
+
+//Pulls up one post by id
+router.get('/posts/:id', withAuth, async (req, res) => { 
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name', 'email', 'url'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true }); 
+
+    res.render('posts', { 
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Create a new post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//Delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+  console.log('req params', req.params.id);
+  try {
+    const postData = await Post.destroy({
+      where: {
+        post_id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
