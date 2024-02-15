3bd53fc13d8c70c0c35a0a5754b90c54bbe8fb60
Juniper <juniperwrenarts@gmail.com>
2024-02-15 09:51:54 -0500

    got Posts to show up on dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,6 +19,8 @@ router.get('/', async (req, res) => {
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
 
+    // console.log(posts);
+
     // Pass serialized data and session flag into template
     res.render('homepage', {
       posts,
@@ -64,7 +66,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
     });
 
     const user = userData.get({ plain: true });
-
+    // console.log(user);
     // Render the 'dashboard' view instead of 'profile'
     res.render('dashboard', {
       ...user,
